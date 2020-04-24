d844d98adf06002f568699133c7d9f156e210786
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-04-24 18:23:29 +0800

    用户注册2.0

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path,include,re_path
-from user import views
+from user.views import RegisterView, ActiveView, LoginView
 from django.conf.urls import  url
+
 app_name = 'user'
 urlpatterns = [
-url(r"^register/$", views.register, name='register'),
-url(r"^register_handle/$", views.register_handle, name="register_handle")
+   url(r"^register/$", RegisterView.as_view()),
+   url(r"^active/$", LoginView.as_view()),
+   url(r"^active/(?P<token>.*)/$", ActiveView.as_view())
 ]

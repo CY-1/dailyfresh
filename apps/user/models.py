06a715787e481989ac22d0ab72eeeb15bccfd537
Y.D.C <45694174+CY-1@users.noreply.github.com>
2020-05-29 11:28:14 +0800

    添加用户头像字段

--- apps/user/models.py
+++ apps/user/models.py
@@ -15,8 +15,12 @@ class AddressManager(models.Manager):
             address = None
         print(address)
         return address
+
+
 class User(AbstractUser, BaseModel):
     '''用户模型类'''
+
+    image = models.ImageField(upload_to='image', verbose_name='头像')
 
     def __str__(self):
         return self.username
